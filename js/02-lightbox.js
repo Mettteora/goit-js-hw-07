c3aa56952f7fc4fa26c9d3b72ba4befccfc35fe4
Mettteora <mrhens2013@gmail.com>
2023-03-27 23:33:49 +0200

    123

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const ulGal = document.querySelector(`.gallery`);
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+       <li class="gallery__item">
+   <a class="gallery__link" href="large-image.jpg">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+    })
+    .join(``);
+}
+
+const test = createGallery(galleryItems);
+ulGal.insertAdjacentHTML("afterbegin", test);
 console.log(galleryItems);
